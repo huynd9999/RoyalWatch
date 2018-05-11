53840696b2648eee1fdad03d86c9bbd7e21ebc3e
MININT-I3K604L\HuyND <huynd.icthust@gmail.com>
2018-05-11 18:27:52 +0700

    First Commit

---
+++ RoyalWatchShop.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoyalWatchShop.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Windows User")]
+[assembly: AssemblyProduct("RoyalWatchShop.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Windows User 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af62c089-bae7-45fc-a9b0-1a42ce422ef5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
